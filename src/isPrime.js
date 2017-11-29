0246628c1056c94224ba0c4c77917ef2041d2d65
Anthony Crisp <anthony_crisp@hotmail.com>
2017-11-29 01:15:56 +0000

    minor syntax tweaks

--- src/isPrime.js
+++ src/isPrime.js
@@ -1,6 +1,6 @@
 module.exports = (n) => {
   if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
   const m = Math.sqrt(n);
-  for (let i = 2; i <= m; i++) if (n % i == 0) return false;
+  for (let i = 2; i <= m; i += 1) if (n % i === 0) return false;
   return true;
 };
